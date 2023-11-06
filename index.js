34a9e630e0ee00715984f1791a1bb8e6d68e1c36
EligibleDev <eligibledev@gmail.com>
2023-11-06 09:57:08 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+
+//DB URI
+const uri =
+    "mongodb+srv://mikailhossain0202:nVmuNWNGl28Sgzic@cluster0.csft7jl.mongodb.net/?retryWrites=true&w=majority";
+
+//connecting mongoDB
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+const run = async () => {
+    try {
+        //   await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        //   await client.close();
+    }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Food Fair!");
