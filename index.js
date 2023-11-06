d42e47387e2883730a632731ab2f385748ea43aa
EligibleDev <eligibledev@gmail.com>
2023-11-06 13:03:21 +0600

    single food api done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 //DB URI
 const uri =
@@ -18,7 +23,50 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
     try {
-        //   await client.connect();
+        //     await client.connect();
+
+        const foodsCollection = client.db("foodFairDB").collection("foods");
+        foodsCollection.createIndex({ foodName: "text" });
+
+        //   jwt
+        app.post("/api/v1/auth/access_token", (req, res) => {});
+
+        //   getting all foods
+        app.get("/api/v1/foods", async (req, res) => {
+            const cursor = foodsCollection.find();
+            const foods = await cursor.toArray();
+
+            res.send(foods);
+        });
+
+        //getting single food
+        app.get("/api/v1/foods/:foodId", async (req, res) => {
+            const foodId = req.params.foodId;
+            const query = { _id: new ObjectId(foodId) };
+
+            const food = await foodsCollection.findOne(query);
+            res.send(food);
+        });
+
+        //getting most sold foods to show on homepage
+        app.get("/api/v1/mostSoldFoods", async (req, res) => {
+            const cursor = foodsCollection.find().sort({ timesSold: -1 });
+            const foundFoods = await cursor.limit(6).toArray();
+
+            res.send(foundFoods);
+        });
+
+        //increasing the timesSold value
+        app.post("/api/v1/foods/increment-sales/:foodId", async (req, res) => {
+            const foodId = req.params.foodId;
+            await foodsCollection.updateOne(
+                { _id: new ObjectId(foodId) },
+                { $inc: { timesSold: 1 } }
+            );
+
+            res.json({ message: "Sales count updated successfully" });
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
